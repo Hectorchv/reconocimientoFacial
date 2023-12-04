e664d41d14fc15edd3a31ee10c17035921b1f1c4
Hectorchv <hectorchv045@gmail.com>
2023-12-04 00:54:38 -0600

    pruebas correctas

--- reconocimiento.py
+++ reconocimiento.py
@@ -2,17 +2,17 @@ import cv2
 import numpy
 import os
 
-face_cascade = cv2.CascadeClassife("haarcascade_frontalface_default.xml")
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 
 while True:
     _, img = cap.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     rostro = face_cascade.detectMultiScale(gray, 1.1, 4)
-    for (x, y, w, h) in rostros:
-        cv2.rectangle(img, (x,y), (x+w, y+h,) (0,255,0), 3)
-    cv2.imgshow('img', img)
-    if cv2.waitKey(30):
+    for (x, y, w, h) in rostro:
+        cv2.rectangle(img, (x,y), (x+w, y+h,), (0,255,0), 3)
+    cv2.imshow('img', img)
+    if cv2.waitKey(30) == True:
         break
 cap.release()
